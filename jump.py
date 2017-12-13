77bab137a80c8519c010b57a54e28e15d85af869
andy.han <andy.han@vipshop.com>
2017-12-13 18:32:59 +0800

    add show env.json

--- jump.py
+++ jump.py
@@ -17,23 +17,39 @@ ENV_LIST = []
 
 
 def main():
-	init_env()
-
-	env = ''
-	user = ''
-	ip = ''
-	port = ''
-	password = ''
-
 	args = sys.argv
 	if len(args) == 1:
 		print('Too less arguments!')
 		return
-	elif len(args) == 2:
-		env = args[1]
-	else:
+	elif len(args) > 2:
 		print('Too many arguments!')
 		return
+
+	init_env()
+
+	if args[1].startswith('--'):
+		learn(args[1][2:])
+		return
+	else:
+		do_conn(args)
+		return
+
+
+def learn(ask_msg):
+	if ask_msg == 'conf':
+		print(json.dumps(ENV_LIST, sort_keys=True, indent=4, separators=(',', ': ')))
+	elif ask_msg == 'env':
+		print(json.dumps(ENV_LIST, sort_keys=True, indent=4, separators=(',', ': ')))
+	else:
+		print('%s is not define, please check your input!' % ask_msg)
+
+
+def do_conn(args):
+	env = args[1]
+	user = ''
+	ip = ''
+	port = ''
+	password = ''
 
 	for env_item in ENV_LIST:
 		if env_item and env_item.get('env') and env == env_item.get('env'):
@@ -68,9 +84,9 @@ def init_env():
 		global ENV_LIST
 		with open(os.path.join(sys.path[0], 'env.json'), 'r') as env_json:
 			ENV_LIST = json.loads(env_json.read())
-
 	except Exception as e:
 		print('load env.json error, please check your file')
+
 
 if __name__ == '__main__':
 	main()
